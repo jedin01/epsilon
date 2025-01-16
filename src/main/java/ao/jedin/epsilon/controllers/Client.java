63472b4cc5a6d37e9a5a256faf9297744de9aeed
jedin01 <abnerjaredejede@gmail.com>
2025-01-16 12:12:29 +0100

    test request

---
+++ src/main/java/ao/jedin/epsilon/controllers/Client.java
@@ -0,0 +1,14 @@
+package ao.jedin.epsilon.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Client {
+    @GetMapping
+    public String howdy(){
+        return "animal se comunicando...";
+    }
+}
