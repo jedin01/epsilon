ad7a3593fd39ebcf24798d66c06576a40370162f
jedin01 <abnerjaredejede@gmail.com>
2025-01-16 11:55:14 +0100

    first commit

---
+++ src/test/java/ao/jedin/epsilon/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ao.jedin.epsilon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
