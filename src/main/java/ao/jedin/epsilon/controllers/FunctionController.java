3bce703fd5dea669dcc1ca31b3626b8e7c9c5e03
jedin01 <abnerjaredejede@gmail.com>
2025-02-25 10:02:47 +0100

    funcontroller changes

--- src/main/java/ao/jedin/epsilon/controllers/FunctionController.java
+++ src/main/java/ao/jedin/epsilon/controllers/FunctionController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@RequestMapping(name = "analizar")
+@RequestMapping(name="/analizar")
 public class FunctionController {
 
     @GetMapping
