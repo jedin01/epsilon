d89b50e15356048c492be46ffb2a781605145faa
jedin01 <abnerjaredejede@gmail.com>
2025-02-25 11:16:39 +0100

    feb25-11:16

--- src/main/java/ao/jedin/epsilon/controllers/FunctionController.java
+++ src/main/java/ao/jedin/epsilon/controllers/FunctionController.java
@@ -1,15 +1,23 @@
 package ao.jedin.epsilon.controllers;
 
 
+import org.matheclipse.core.eval.ExprEvaluator;
+import org.matheclipse.core.interfaces.IExpr;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping(name="/analizar")
+@RestController
+@RequestMapping("/analisar")
 public class FunctionController {
 
     @GetMapping
-    public String analisar(@RequestParam String expression){
-        return "Animal se comunicando...";
+    public String analisar(@RequestParam String expre){
+
+        ExprEvaluator engine = new ExprEvaluator();
+
+        IExpr integration = engine.evaluate("Integrate("+expre+",x)");
+        return "result: "+engine.evaluate("Expand("+integration+")");
     }
 }
