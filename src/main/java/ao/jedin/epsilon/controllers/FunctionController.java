62ae8963cab70c621d9e2eb9678664862463f8f4
jedin01 <abnerjaredejede@gmail.com>
2025-02-25 09:58:14 +0100

    defina a class FuctionController

---
+++ src/main/java/ao/jedin/epsilon/controllers/FunctionController.java
@@ -0,0 +1,15 @@
+package ao.jedin.epsilon.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequestMapping(name = "analizar")
+public class FunctionController {
+
+    @GetMapping
+    public String analisar(@RequestParam String expression){
+        return "Animal se comunicando...";
+    }
+}
