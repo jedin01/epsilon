13d6fc3c75bc88083372c6d220b819e44355f155
jedin01 <abnerjaredejede@gmail.com>
2025-04-02 13:33:56 +0100

    something at Apr02 13:33

--- src/main/java/ao/jedin/epsilon/controllers/FunctionController.java
+++ src/main/java/ao/jedin/epsilon/controllers/FunctionController.java
@@ -3,21 +3,20 @@ package ao.jedin.epsilon.controllers;
 
 import org.matheclipse.core.eval.ExprEvaluator;
 import org.matheclipse.core.interfaces.IExpr;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
-@RequestMapping("/analisar")
-public class FunctionController {
+@RequestMapping("/")
+public class FunctionController{
 
-    @GetMapping
-    public String analisar(@RequestParam String expre){
+    @GetMapping("solve/")
+    public ResponseEntity<List<String>> solve(@)
 
-        ExprEvaluator engine = new ExprEvaluator();
-
-        IExpr integration = engine.evaluate("Integrate("+expre+",x)");
-        return "result: "+engine.evaluate("Expand("+integration+")");
-    }
 }
