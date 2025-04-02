aca0f5da1bc0a71e948a03fa2e889ba21c35c213
jedin01 <abnerjaredejede@gmail.com>
2025-04-02 14:21:51 +0100

    apr 02 14:21

--- src/main/java/ao/jedin/epsilon/controllers/FunctionController.java
+++ src/main/java/ao/jedin/epsilon/controllers/FunctionController.java
@@ -3,6 +3,7 @@ package ao.jedin.epsilon.controllers;
 
 import org.matheclipse.core.eval.ExprEvaluator;
 import org.matheclipse.core.interfaces.IExpr;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +11,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
 @RequestMapping("/")
 public class FunctionController{
 
-    @GetMapping("solve/")
-    public ResponseEntity<List<String>> solve(@)
+    @GetMapping("/solve")
+    public ResponseEntity<HashMap<String, Object>> solve(@RequestParam(required = false) String expression){
+        HashMap<String, Object> response = FunctionStud
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 
 }
